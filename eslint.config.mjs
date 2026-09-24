3e3147aaf0de94ea1f4e693ee7c368fb22759dc0
Davide Di Criscito <dcdavidev@gmail.com>
2026-09-24 16:56:14 +0200

    chore: configure vscode extensions and ignore react-router in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,7 @@ export default defineConfig([
     '**/.next/**',
     '**/.nuxt/**',
     '**/.svelte-kit/**',
+    '**/.react-router/**',
     '**/.turbo/**',
     '**/.pnpm/**',
     '**/.npm/**',
